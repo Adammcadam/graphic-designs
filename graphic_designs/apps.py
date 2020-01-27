4b9cec66537e66a3f315fd117eb0f282d942776d
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-01-27 18:57:51 +0000

    initial commit

---
+++ graphic_designs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GraphicDesignsConfig(AppConfig):
+    name = 'graphic_designs'
---
+++ graphic_designs/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='graphic_designs-home'),
+    path('/about', views.about, name='graphic_designs-about'),
+]
