503cc026a608631d0d0e2e5e284b82cc541fc49e
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-01-29 19:25:57 +0000

    created 2 models for post and testimonials

--- graphic_designs/models.py
+++ graphic_designs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    # deletes the post if a user is deleted
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Testimonial(models.Model):
+    header = models.CharField(max_length=100)
+    testimonial = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
