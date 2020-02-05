eef60d2f708b04d0f0c16a8928c50ea3e40e87bc
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-02-05 19:36:46 +0000

    added user accounts

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    # make sure account is deleted if user is deleted
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='account_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Account'
+
