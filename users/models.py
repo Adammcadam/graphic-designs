f6bd05c9e94dc068bed2a8170fec0f7796009dd7
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-02-05 20:20:07 +0000

    added image resizing

--- users/models.py
+++ users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Account(models.Model):
     # make sure account is deleted if user is deleted
@@ -8,4 +9,14 @@ class Account(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Account'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
