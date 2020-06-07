f58cc3ac5da214c4a2efcdf65a05ce3c4fb30ee0
Adam Wragg <adamwragg@Adams-MacBook-Air.local>
2020-06-07 17:26:01 +0100

    fixed mistake and changed order admin view

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Account, Item, Order, OrderItem, Payment
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'ordered']
+
 admin.site.register(Account)
 admin.site.register(Item)
 admin.site.register(OrderItem)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
